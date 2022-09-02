f4679901b6017fb655d2c1c1de4925193ac2a64a
micaellagouveia <micaella2212@gmail.com>
2022-09-02 17:52:23 -0300

    step 6: configure backend cd to heroku

--- library_back/src/settings.py
+++ library_back/src/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,3 +154,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = "/static/"
 STATIC_URL = "/static/"
+django_heroku.settings(locals())
