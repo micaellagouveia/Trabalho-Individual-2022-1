e1b9b8bafa0599a4ae170b1c768aa363bcc39618
micaellagouveia <micaella2212@gmail.com>
2022-09-04 19:34:41 -0300

    step 4: add front ci

--- docker/front.Dockerfile
+++ docker/front.Dockerfile
@@ -4,6 +4,6 @@ COPY library_front /library_front
 
 WORKDIR /library_front
 
-RUN apt update -y && apt install npm -y
+RUN yarn install
 
-RUN npm install
+CMD ["yarn", "start"]
--- library_front/src/App.test.js
+++ library_front/src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders learn react link', async() => {
+  const { findByText } = render(<App />);
+  expect (await findByText('Biblioteca')).toBeTruthy()
+  expect (await findByText('Perfil')).toBeTruthy()
+  expect (await findByText('Registro')).toBeTruthy()
+  expect (await findByText('Bem vindo a Biblioteca')).toBeTruthy()
+  expect (await findByText('Livros disponíveis')).toBeTruthy()
+  expect (await findByText('Doar livro')).toBeTruthy()
 });
