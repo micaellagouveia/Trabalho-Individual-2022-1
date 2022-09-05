be6f0d507206dc5137854ea23ccc4a494b7fc7e7
micaellagouveia <micaella2212@gmail.com>
2022-09-04 21:04:37 -0300

    step 6: add front cd

--- library_front/src/Services/Axios/baseService.js
+++ library_front/src/Services/Axios/baseService.js
@@ -1,5 +1,12 @@
 import axios from "axios";
 
+function getEnvVar(varName, defaultValue) {
+    const result = process.env[varName];
+
+    if (result !=undefined) return result;
+    return defaultValue;
+}
+
 export const BASE_API = axios.create({
-    baseURL: "http://localhost:82"
+    baseURL: getEnvVar(BACKEND_HOST, 'http://localhost:82')
 });
